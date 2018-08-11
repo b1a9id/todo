0d3ea66ba755b35a6eb804c758e101e8e7484daa
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2018-08-11 16:18:21 +0900

    add index controller

---
+++ src/main/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/b1a9id/todo/src/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func main()  {
+	router := gin.Default()
+
+	router.GET("/", controller.IndexGET)
+	router.Run(":8080")
+}
